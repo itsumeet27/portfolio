4755583f38c33ae0cd2e3286a36893be9616bd46
Sumeet Sharma <sksksharma0@gmail.com>
2019-11-22 15:13:47 +0530

    Adding Fonts

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Web Developer | Sumeet Sharma</title>
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins|Righteous&display=swap" rel="stylesheet">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
   <!-- Bootstrap core CSS -->
