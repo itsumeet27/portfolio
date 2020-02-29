f895d96cebbf8476632ad0f59b19cc6267ba49b2
Sumeet Sharma <sksksharma0@gmail.com>
2020-03-01 00:28:33 +0530

    Portfolio Changes

--- portfolio.php
+++ portfolio.php
@@ -7,55 +7,318 @@
 			<h1 class="h1-responsive">Portfolio</h1>
 			<hr class="section">
 		</div>
-		<div class="row">
-			<div class="col-md-4 animated fadeInLeft">
-				<!-- Card -->
-				<div class="card card-image m-2">
-					<!-- Content -->
-					<img src="img/portfolio/amittradersindia.png" />
-					<div class="card-overlay p-4">
-						<h5 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h5>
-						<h3 class="card-title pt-2"><strong>Amit Traders India</strong></h3>
-						<p class="text-justify">Manufacturers and Suppliers of Leather goods and Corporate gift articles </p>
-						<a class="btn btn-outline-black" href="https://amittradersindia.com"><i class="fas fa-clone left"></i>
-						View project</a>
-					</div>
-				</div>
-				<!-- Card -->
-			</div>
-			<div class="col-md-4 animated fadeInUp">
-				<!-- Card -->
-				<div class="card card-image m-2">
-					<!-- Content -->
-					<img src="img/portfolio/khadipremiumcosmetics.png" />
-					<div class="card-overlay p-4">
-						<h5 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h5>
-						<h3 class="card-title pt-2"><strong>Khadi Premium Cosmetics</strong></h3>
-						<p class="text-justify">A wide range of natural and herbal cosmetic
-						products to take care for your skin, hair and body. </p>
-						<a class="btn btn-outline-black" href="https://khadipremiumcosmetics.com"><i
-							class="fas fa-clone left"></i>
-						View project</a>
-					</div>
-				</div>
-				<!-- Card -->
-			</div>
-			<div class="col-md-4 animated fadeInRight">
-				<!-- Card -->
-				<div class="card card-image m-2">
-					<!-- Content -->
-					<img src="img/portfolio/filaydent.png" />
-					<div class="card-overlay p-4">
-						<h5 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h5>
-						<h3 class="card-title pt-2"><strong>FilayDent</strong></h3>
-						<p class="text-justify">An E-Commerce website for instruments used in dental surgery </p>
-						<a class="btn btn-outline-black" href="https://filaydent.com"><i class="fas fa-clone left"></i>
-						View project</a>
-					</div>
-				</div>
-				<!-- Card -->
-			</div>
-		</div>
+		<ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="all-tab" data-toggle="tab" href="#all" role="tab" aria-controls="all"
+              aria-selected="true">All</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="web-tab" data-toggle="tab" href="#web" role="tab" aria-controls="web"
+              aria-selected="false">Web Development<a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="design-tab" data-toggle="tab" href="#design" role="tab" aria-controls="design"
+              aria-selected="false">Designing</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">
+            <div class="web mt-4">
+              <div class="row pt-3">
+                <div class="col-md-4 animated fadeInLeft">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/amittradersindia.png"
+                      style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h6>
+                      <h3 class="card-title pt-2"><strong>Amit Traders India</strong></h3>
+                      <p class="text-justify">Manufacturers and Suppliers of Leather goods and Corporate gift articles
+                      </p>
+                      <a class="btn btn-outline-black" href="https://amittradersindia.com"><i
+                          class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeInUp">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/khadipremiumcosmetics.png"
+                      style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h6>
+                      <h3 class="card-title pt-2"><strong>Khadi Premium Cosmetics</strong></h3>
+                      <p class="text-justify">A wide range of natural and herbal cosmetic
+                        products to take care for your skin, hair and body. </p>
+                      <a class="btn btn-outline-black" href="https://khadipremiumcosmetics.com"><i
+                          class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeInRight">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/filaydent.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h6>
+                      <h3 class="card-title pt-2"><strong>FilayDent</strong></h3>
+                      <p class="text-justify">An E-Commerce website for instruments used in dental surgery </p>
+                      <a class="btn btn-outline-black" href="https://filaydent.com"><i class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+              </div>
+			  <div class="row pt-3">
+                <div class="col-md-4 animated fadeInLeft">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/fashionst.png"
+                      style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-person-booth"></i> Event</h6>
+                      <h3 class="card-title pt-2"><strong>FashionST</strong></h3>
+                      <p class="text-justify">A ramp for fashion passionates who want to start their career in the field of modelling.
+                      </p>
+                      <a class="btn btn-outline-black" href="https://fashionst.org"><i
+                          class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeInUp">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/khadipremiumcosmetics-boilerplate.png"
+                      style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h6>
+                      <h3 class="card-title pt-2"><strong>Khadi Premium Cosmetics</strong></h3>
+                      <p class="text-justify">A wide range of natural and herbal cosmetic
+                        products to take care for your skin, hair and body. </p>
+                      <a class="btn btn-outline-black" href="http://itsumeet.com/khadipremiumcosmetics"><i
+                          class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeInRight">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/women-empowerment.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-user-graduate"></i> Portal</h6>
+                      <h3 class="card-title pt-2"><strong>Women Empowerment</strong></h3>
+                      <p class="text-justify">A PHP Web Application Portal developed for the women who want to start their career belonging to any background and work independently by learning various courses provided by different NGO(s) under the STEP Scheme. </p>
+                      <a class="btn btn-outline-black" href="http://itsumeet.com/women-empowerment"><i class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+              </div>
+            </div>
+            <div class="design mt-4">
+              <div class="row pt-3">
+                <div class="col-md-4 animated fadeIn">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/design/Business Card.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h3 class="card-title pt-2"><strong>Business Card</strong></h3>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeIn">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/design/Identity Card.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h3 class="card-title pt-2"><strong>Identity Card</strong></h3>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeIn">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/design/Profile Banner.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h3 class="card-title pt-2"><strong>Profile Banner</strong></h3>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="web" role="tabpanel" aria-labelledby="web-tab">
+            <div class="web mt-4">
+              <div class="row pt-3">
+                <div class="col-md-4 animated fadeInLeft">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/amittradersindia.png"
+                      style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h6>
+                      <h3 class="card-title pt-2"><strong>Amit Traders India</strong></h3>
+                      <p class="text-justify">Manufacturers and Suppliers of Leather goods and Corporate gift articles
+                      </p>
+                      <a class="btn btn-outline-black" href="https://amittradersindia.com"><i
+                          class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeInUp">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/khadipremiumcosmetics.png"
+                      style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h6>
+                      <h3 class="card-title pt-2"><strong>Khadi Premium Cosmetics</strong></h3>
+                      <p class="text-justify">A wide range of natural and herbal cosmetic
+                        products to take care for your skin, hair and body. </p>
+                      <a class="btn btn-outline-black" href="https://khadipremiumcosmetics.com"><i
+                          class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeInRight">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/filaydent.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h6>
+                      <h3 class="card-title pt-2"><strong>FilayDent</strong></h3>
+                      <p class="text-justify">An E-Commerce website for instruments used in dental surgery </p>
+                      <a class="btn btn-outline-black" href="https://filaydent.com"><i class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+              </div>
+			  <div class="row pt-3">
+                <div class="col-md-4 animated fadeInLeft">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/fashionst.png"
+                      style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-person-booth"></i> Event</h6>
+                      <h3 class="card-title pt-2"><strong>FashionST</strong></h3>
+                      <p class="text-justify">A ramp for fashion passionates who want to start their career in the field of modelling.
+                      </p>
+                      <a class="btn btn-outline-black" href="https://fashionst.org"><i
+                          class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeInUp">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/khadipremiumcosmetics-boilerplate.png"
+                      style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-shopping-cart"></i> E-Commerce</h6>
+                      <h3 class="card-title pt-2"><strong>Khadi Premium Cosmetics</strong></h3>
+                      <p class="text-justify">A wide range of natural and herbal cosmetic
+                        products to take care for your skin, hair and body. </p>
+                      <a class="btn btn-outline-black" href="http://itsumeet.com/khadipremiumcosmetics"><i
+                          class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeInRight">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/portfolio/women-empowerment.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h6 class="black-text"><i class="fas fa-user-graduate"></i> Portal</h6>
+                      <h3 class="card-title pt-2"><strong>Women Empowerment</strong></h3>
+                      <p class="text-justify">A PHP Web Application Portal developed for the women who want to start their career belonging to any background and work independently by learning various courses provided by different NGO(s) under the STEP Scheme. </p>
+                      <a class="btn btn-outline-black" href="http://itsumeet.com/women-empowerment"><i class="fas fa-clone left"></i>
+                        View project</a>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="design" role="tabpanel" aria-labelledby="design-tab">
+            <div class="design mt-4">
+              <div class="row pt-3">
+                <div class="col-md-4 animated fadeIn">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/design/Business Card.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h3 class="card-title pt-2"><strong>Business Card</strong></h3>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeIn">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/design/Identity Card.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h3 class="card-title pt-2"><strong>Identity Card</strong></h3>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+                <div class="col-md-4 animated fadeIn">
+                  <!-- Card -->
+                  <div class="card card-image" style="height: 100%">
+                    <!-- Content -->
+                    <img src="img/design/Profile Banner.png" style="width: 100%" />
+                    <div class="card-overlay p-4">
+                      <h3 class="card-title pt-2"><strong>Profile Banner</strong></h3>
+                    </div>
+                  </div>
+                  <!-- Card -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 	</section>
 </div>
 <?php include ('includes/footer.php'); ?>
