3c8f9c5be3df6519b56b2e8c83eb307dde57bd71
Sumeet Sharma <sksksharma0@gmail.com>
2020-06-15 13:12:00 +0530

    Adding My Drawings

---
+++ drawings.php
@@ -0,0 +1,15 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="description" content="Continuing my childhood hobby that makes my mind calm and acts as a relief for a busy day. Sharing with you'll my drawings. I hope you lke it!">
+  <title>Portfolio | Sumeet Sharma - Software Engineer | UI/Frontend Developer | Web Developer</title>
+  <link rel="canonical" href="https://itsumeet.com/">
+</head>
+<?php include ('includes/header.php'); ?>
+
+	<div class="container-fluid">
+
+	</div>
+
+<?php include ('includes/footer.php'); ?>
